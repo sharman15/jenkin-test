a934d849dda203fd6463e68fc553ebbee22ab13e
sharman <narayan.kumar@tomtom.com>
2016-12-03 17:31:18 +0530

    Maven branch

--- abc.java
+++
@@ -1,5 +0,0 @@
-public class abc {
- public static void main(String [] str) {
-  System.out.println("Welcome for third build !");
-}
-}
--- hello.java
+++
@@ -1,5 +0,0 @@
-public class hello {
- public static void main(String [] str) {
-  System.out.println("Hello World !");
-}
-}
